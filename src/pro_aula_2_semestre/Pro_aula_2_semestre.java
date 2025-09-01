a2795a4d04a27244718460c156b4454ef85b8027
AJEC1804 <alejandrocastellar04@gmail.com>
2025-09-01 13:46:46 -0500

    primer commit

---
+++ src/pro_aula_2_semestre/Pro_aula_2_semestre.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pro_aula_2_semestre;
+
+/**
+ *
+ * @author Jarvi
+ */
+public class Pro_aula_2_semestre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
